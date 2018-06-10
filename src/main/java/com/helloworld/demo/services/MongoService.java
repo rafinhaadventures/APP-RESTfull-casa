1d56ce9f528ab249a1a63dcbfc0f19f57441064c
Rafa Alvarez Vega <r.alve93@gmail.com>
2018-06-10 03:04:44 +0200

    TODO Modelo de Datos

--- src/main/java/com/helloworld/demo/services/MongoService.java
+++ src/main/java/com/helloworld/demo/services/MongoService.java
@@ -4,17 +4,28 @@ import org.apache.catalina.core.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.helloworld.demo.configuration.MongoConfiguration;
 
 public class MongoService {
-	private MongoOperations mongoOperation;
-	
-	public MongoService() {
+	static {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfiguration.class);
 	    mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");
 	}
+	private static MongoOperations mongoOperation;
 
-	//TODO Generar Metodos CRUD
+	//TODO Generar Metodos CRUD - con el modelo de datos
+	
+	public static void create(Object s) {
+		mongoOperation.save(s);
+	}
+	
+	public static void read(Object s) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("clave").in("valor"));
+		mongoOperation.find(query, null //modelo de datos);
+	}
 
 }
